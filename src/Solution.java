d6a802d2b1336f0fe0d5ae47a4deb81b47f169bb
Ansar Ismagilov <ansar89kab@bk.ru>
2023-04-26 13:44:46 +0700

    accepted my solution

--- src/Solution.java
+++ src/Solution.java
@@ -4,16 +4,10 @@ class Solution { // "abb"
         String temp = "";
         if (s.length() <= 1) return s;
         int iL, iR;
-//-----
+        //-----
         for (int i = 1; i < s.length(); i++) {
-            if (s.length() % 2 == 0) {
-                iR = i;
-                iL = i - 1;
-            } else {
-                temp = String.valueOf(s.charAt(i));
-                iR = i + 1;
-                iL = i - 1;
-            }
+            iR = i;
+            iL = i - 1;
             while (iL >= 0 && iR < s.length()) {
                 if (s.charAt(iL) == s.charAt(iR)) {
                     temp = s.charAt(iL) + temp + s.charAt(iR);
@@ -25,29 +19,25 @@ class Solution { // "abb"
             temp = "";
         }
         //-----
-        for (int i = 1; i < s.length(); i++) {
-            if (s.length() % 2 == 0) {
-                iR = i;
-                iL = i - 1;
-            } else {
+        if (s.length() >= 3) {
+            for (int i = 1; i < s.length(); i++) {
                 temp = String.valueOf(s.charAt(i));
                 iR = i + 1;
                 iL = i - 1;
+                while (iL >= 0 && iR < s.length()) {
+                    if (s.charAt(iL) == s.charAt(iR)) {
+                        temp = s.charAt(iL) + temp + s.charAt(iR);
+                        iL -= 1;
+                        iR += 1;
+                    } else break;
+                }
+                if (temp.length() > res.length()) res = temp;
+                temp = "";
             }
-            while (iL >= 0 && iR < s.length()) {
-                if (s.charAt(iL) == s.charAt(iR)) {
-                    temp = s.charAt(iL) + temp + s.charAt(iR);
-                    iL -= 1;
-                    iR += 1;
-                } else break;
-            }
-            if (temp.length() > res.length()) res = temp;
-            temp = "";
         }
         //------
 
-        if(res.length() == 0) return String.valueOf(s.charAt(0));
-
+        if (res.length() == 0) return String.valueOf(s.charAt(0));
         return res;
     }
 }
